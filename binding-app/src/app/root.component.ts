d481617a121b5337cd7bfb0e62c62f3fd208acbb
Rajendhar <rajendhar.mamidala@capgemini.com>
2017-07-06 07:40:26 +0530

    Initial files added

---
+++ binding-app/src/app/root.component.ts
@@ -0,0 +1,13 @@
+// Importing Component Module from Angular core.
+import { Component } from "@angular/core"
+
+// Decorator component for RootComponent class
+@Component({
+    selector: 'my-app-component',
+    templateUrl: './root.component.html'
+})
+
+// Class Root Component.
+export class RootComponent {
+
+}
