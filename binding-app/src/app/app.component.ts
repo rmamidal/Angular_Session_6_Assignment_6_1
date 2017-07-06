947b6f9aad425411648d583bdc9b2df4bf1c48c9
Rajendhar <rajendhar.mamidala@capgemini.com>
2017-07-06 21:55:16 +0530

    Angularjs - Interpolation Binding example

--- binding-app/src/app/app.component.ts
+++ binding-app/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+// Importing component and OnInit modules
+import { Component, OnInit } from '@angular/core';
 
+// Component Decorator
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+
+  /**Public variable */
+  title: string;
+  name: string;
+
+  ngOnInit() {
+    /** Assign values to the variable after OnInit */
+    this.title = 'My Angular Binding Application';
+    this.name = 'Raj';
+  }
+
 }
--- binding-app/src/app/app.module.ts
+++ binding-app/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { RootComponent } from './root.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RootComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
